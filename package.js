e817a9277c633579ffa9f533bae5649927d10968
andy <andy@Goddard.local>
2016-10-27 16:57:38 -0700

    bump version

--- package.js
+++ package.js
@@ -2,7 +2,7 @@
 
 Package.describe({
   summary: 'Login service for Stripe accounts',
-  version: '0.1.4',
+  version: '0.1.5',
   name: 'billyvg:accounts-stripe',
   git: 'https://github.com/Opstarts/meteor-accounts-stripe.git',
 });
@@ -16,7 +16,7 @@ Package.onUse(function(api) {
   api.imply('accounts-base', ['client', 'server']);
   api.use('accounts-oauth', ['client', 'server']);
   api.use('service-configuration', ['client', 'server']);
-  api.use('billyvg:stripe-oauth@0.1.4', ['client', 'server']);
+  api.use('billyvg:stripe-oauth@0.1.5', ['client', 'server']);
 
   api.use('http', ['server']);
 
